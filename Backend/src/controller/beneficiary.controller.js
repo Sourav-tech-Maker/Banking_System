288f82b893ebba6a8d4f065b197b91f2bffe214e
Sourav-tech-Maker <souravv.mailbox@gmail.com>
2026-06-24 16:01:34 +0530

    remove comments

--- Backend/src/controller/beneficiary.controller.js
+++ Backend/src/controller/beneficiary.controller.js
@@ -4,10 +4,6 @@ const accountModel = require('../models/account.model');
 const { sendEmail } = require('../services/email.service');
 const { generateOtp, getOtpHtml, getOtpText} = require('../Utils/otp.utils')
 
-// const generateOtp = () => Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit OTP string
-// const getOtpText = (otp) => `Your verification code to add a beneficiary is: ${otp}`;
-// const getOtpHtml = (otp) => `<h3>Beneficiary Verification</h3><p>Your code is: <strong>${otp}</strong></p>`;
-
 async function addBeneficiaries(req, res) {
     try {
         const { fullName, nickName, accountId, email } = req.body || {}; 
